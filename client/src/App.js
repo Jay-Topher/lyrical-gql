818b3b4efc31372b608323812e0d028031f03066
Jones Ogolo <jay.topher11@gmail.com>
2020-05-02 20:12:23 +0100

    setup apollo client on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from '@apollo/react-hooks'
+// import ApolloClient from "apollo-client";
+// import { ApolloProvider } from "react-apollo";
+import "./App.css";
+import Routes from "./Routes";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+// there are two kinds of people in this world
+// 1. Those who can extrapolate from incomplete data.
